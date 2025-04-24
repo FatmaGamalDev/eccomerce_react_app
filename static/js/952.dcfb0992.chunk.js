580d6d8949769c1c05a99c188665872668d10752
fatma gamal <fgmal019@gmail.com>
2025-04-24 18:32:31 +0200

    Updates

--- static/js/952.dcfb0992.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkecommerce_app=self.webpackChunkecommerce_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.dcfb0992.chunk.js.map
